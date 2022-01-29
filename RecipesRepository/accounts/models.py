b0bac200568bc0cea8fb9d7db004309dd9eac425
FilipWojszczak <filip.wojszczak@gmail.com>
2022-01-29 18:30:34 +0100

    Start implementing registration with email confirmation

--- RecipesRepository/accounts/models.py
+++ RecipesRepository/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+# from django.contrib.auth import urls
 
-# Create your models here.
+
+class User(AbstractUser):
+    email_confirmed = models.BooleanField(default=False)
+    is_active = models.BooleanField(
+        _('active'),
+        default=False,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
--- RecipesRepository/accounts/views.py
+++ RecipesRepository/accounts/views.py
@@ -1,9 +1,27 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from .tokens import account_activation_token
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your RecipesRepository Account'
+            message = render_to_string('account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect('account_activation_sent')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
