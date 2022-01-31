d8ad866cac2c708edf45621b48f6dc4150d1167a
FilipWojszczak <filip.wojszczak@gmail.com>
2022-01-31 21:46:52 +0100

    Implement registration with email confirmation

--- RecipesRepository/accounts/urls.py
+++ RecipesRepository/accounts/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import signup
+from django.urls import path, re_path
+from .views import signup, account_activation_sent, activate
 
 
 urlpatterns = [
-    path('signup/', signup, name='signup')
+    path('signup/', signup, name='signup'),
+    re_path(r'^account_activation_sent/$', account_activation_sent, name='account_activation_sent'),
+    re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate, name='activate'),
 ]
--- RecipesRepository/accounts/views.py
+++ RecipesRepository/accounts/views.py
@@ -2,9 +2,11 @@ from .forms import SignUpForm
 from django.shortcuts import render, redirect
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
 from .tokens import account_activation_token
+from .models import User
+from django.contrib.auth import login
 
 
 def signup(request):
@@ -14,7 +16,7 @@ def signup(request):
             user = form.save()
             current_site = get_current_site(request)
             subject = 'Activate Your RecipesRepository Account'
-            message = render_to_string('account_activation_email.html', {
+            message = render_to_string('registration/account_activation_email.html', {
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
@@ -24,4 +26,25 @@ def signup(request):
             return redirect('account_activation_sent')
     else:
         form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def account_activation_sent(request):
+    return render(request, 'registration/account_activation_sent.html')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
+        user.save()
+        login(request, user)
+        return redirect('recipes:home')
+    else:
+        return render(request, 'account_activation_invalid.html')
