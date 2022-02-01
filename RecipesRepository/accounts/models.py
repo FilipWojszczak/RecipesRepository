ba777d2d7dd4583af35546a6a944d3f4af47924b
FilipWojszczak <filip.wojszczak@gmail.com>
2022-02-01 21:31:45 +0100

    Set is_active to False only for Users created with signup view

--- RecipesRepository/accounts/models.py
+++ RecipesRepository/accounts/models.py
@@ -6,11 +6,3 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     email_confirmed = models.BooleanField(default=False)
-    is_active = models.BooleanField(
-        _('active'),
-        default=False,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
